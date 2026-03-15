7cbb5434ad14c0148bf3bd85366336826d24eda5
Mominur Rahman <150646318+khmominur@users.noreply.github.com>
2026-03-15 19:19:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function calculateFare() {
+        let gross_fare = parseFloat(document.getElementById('gross_fare').value);
+        let base_fare = parseFloat(document.getElementById('base_fare').value);
+        let iata_com = parseFloat(document.getElementById('iata_com').value);
+        
+        let markup_input = document.getElementById('markup_amount').value;
+        let markup = markup_input === "" ? 0 : parseFloat(markup_input);
+
+        if (isNaN(gross_fare) || isNaN(base_fare) || isNaN(iata_com)) {
+            alert("Please enter Gross Fare, Base Fare and Commission.");
+            return;
+        }
+
+        let AIT = (gross_fare * 0.3) / 100;
+        let IATA_Commission = (base_fare * iata_com) / 100;
+        let net_fare = (gross_fare + AIT - IATA_Commission) + markup;
+
+        document.getElementById('ait').innerText = "AIT (0.3%): " + AIT.toFixed(2);
+        document.getElementById('iata').innerText = "IATA Commission: " + IATA_Commission.toFixed(2);
+        document.getElementById('net').innerText = "Net Fare: " + net_fare.toFixed(2);
+
+        document.getElementById('results').style.display = "block";
+    }
+
+    function resetForm() {
+        // সব ইনপুট ক্লিয়ার করা
+        document.getElementById('gross_fare').value = "";
+        document.getElementById('base_fare').value = "";
+        document.getElementById('iata_com').value = "";
+        document.getElementById('markup_amount').value = "";
+        
+        // রেজাল্ট সেকশন হাইড করা
+        document.getElementById('results').style.display = "none";
+    }
